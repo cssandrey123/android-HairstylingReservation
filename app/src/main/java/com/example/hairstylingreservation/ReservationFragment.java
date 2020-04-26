24dc3c3f5e9ffc5f68bd0222543886e1c40581f3
iulian <raduicaiulian@gmail.com>
2020-04-26 23:46:03 +0300

    Fa o rezervare - update

--- app/src/main/java/com/example/hairstylingreservation/ReservationFragment.java
+++ app/src/main/java/com/example/hairstylingreservation/ReservationFragment.java
@@ -1,19 +1,57 @@
 package com.example.hairstylingreservation;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+
+import static android.content.ContentValues.TAG;
+
 public class ReservationFragment extends Fragment {
-    @Nullable
+
+    private TextView dataDisp;
+    private DatePickerDialog.OnDateSetListener dateListner;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reservation,container,false);
+
+        dataDisp = (TextView) view.findViewById(R.id.date);
+
+        dataDisp.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(getContext(), android.R.style.Theme_Holo_Light_Dialog_MinWidth,dateListner,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        dateListner = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG,"onDateSert: mm/dd/yyy: " + month + "/" + day + "/" + year);
+                String date = month + "/" + day + "/" + year;
+                dataDisp.setText(date);
+            }
+        };
+
         return view;
     }
 }
